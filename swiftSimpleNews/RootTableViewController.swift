9979bf86b027a3f1fd1c39871e933286bc3f6141
Chi Zhang <zhangchi1213@gmail.com>
2014-07-23 10:51:33 +0800

    add swiftSimpleNews loadingmorefooterview

--- swiftSimpleNews/RootTableViewController.swift
+++ swiftSimpleNews/RootTableViewController.swift
@@ -16,15 +16,29 @@
 
 import UIKit
 
-class RootTableViewController: UITableViewController {
+class RootTableViewController: UITableViewController, LoadMoreTableFooterViewDelegate {
     
-    var dataSource = []
+    var dataSource = NSMutableArray()
     
     var thumbQueue = NSOperationQueue()
     
     var pageNo = 1
     
-//    var hackerNewsApiUrl:String =
+    let PAGESIZE = 10
+    
+    var loadMoreFooterView: LoadMoreTableFooterView?
+    var loadingMore: Bool = false
+    var loadingMoreShowing: Bool = false
+    
+//    init(coder aDecoder: NSCoder!) {
+//        super.init(coder: aDecoder)
+//        println("init coder")
+//    }
+//
+//    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//        println("init name")
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,17 +51,50 @@ class RootTableViewController: UITableViewController {
         
         
         self.tableView!.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
+
         let refreshControl = UIRefreshControl()
         refreshControl.attributedTitle = NSAttributedString(string: "下拉刷新")
         refreshControl.addTarget(self, action: "loadDataSource", forControlEvents: UIControlEvents.ValueChanged)
         self.refreshControl = refreshControl
-        
-        
-        
-        loadDataSource()
-        
+
+        if self.loadMoreFooterView == nil {
+            println("contentSize \(self.tableView.contentSize.height) framewidth = \(self.tableView.frame.size.width) frameheight= \(self.tableView.frame.size.height)")
+            self.loadMoreFooterView = LoadMoreTableFooterView(frame: CGRectMake(0, self.tableView.contentSize.height, self.tableView.frame.width, self.tableView.frame.height))
+            self.loadMoreFooterView!.delegate = self
+            self.tableView.addSubview(self.loadMoreFooterView)
+        }
+
+        loadDataSource(true)
     }
+    
+    // LoadMoreTableFooterViewDelegate
+    func loadMoreTableFooterDidTriggerRefresh(view: LoadMoreTableFooterView) {
+//        loadMoreTableViewDataSource()
+        self.pageNo++
+        loadingMore = true
+        loadDataSource(false)
+        println("loadMoreTableFooterDidTriggerRefresh")
+    }
+    
+    func loadMoreTableFooterDataSourceIsLoading(view: LoadMoreTableFooterView) -> Bool {
+        return loadingMore
+    }
+
+    override// UIScrollViewDelegate
+    func scrollViewDidScroll(scrollView: UIScrollView!)
+    {
+        if (loadingMoreShowing) {
+            loadMoreFooterView!.loadMoreScrollViewDidScroll(scrollView)
+        }
+    }
+    
+    override func scrollViewDidEndDragging(scrollView: UIScrollView!, willDecelerate decelerate: Bool) {
+        
+        if (loadingMoreShowing) {
+            loadMoreFooterView!.loadMoreScrollViewDidEndDragging(scrollView)
+        }
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -60,7 +107,7 @@ class RootTableViewController: UITableViewController {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         
-        return dataSource.count
+        return 1
     }
     
     override func tableView(tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
@@ -72,7 +119,14 @@ class RootTableViewController: UITableViewController {
         
     }
     
-    func loadDataSource() {
+    func refreshSource() {
+        pageNo = 1
+        self.dataSource.removeAllObjects()
+        loadDataSource(true)
+    }
+    
+    func loadDataSource(isRefresh : Bool) {
+        println("loadDataSource \(isRefresh)")
         self.refreshControl.beginRefreshing()
         var loadURL = NSURL.URLWithString("http://qingbin.sinaapp.com/api/lists?ntype=%E5%9B%BE%E7%89%87&pageNo=\(pageNo)&pagePer=10&list.htm")
         var request = NSURLRequest(URL: loadURL)
@@ -88,52 +142,72 @@ class RootTableViewController: UITableViewController {
                 let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
                 let newsDataSource = json["item"] as NSArray
                 
-                var currentNewsDataSource = NSMutableArray()
+//                var currentNewsDataSource = NSMutableArray()
                 for currentNews : AnyObject in newsDataSource {
                     let newsItem = XHNewsItem()
                     newsItem.newsTitle = currentNews["title"] as NSString
                     newsItem.newsThumb = currentNews["thumb"] as NSString
                     newsItem.newsID = currentNews["id"] as NSString
-                    currentNewsDataSource.addObject(newsItem)
+                    self.dataSource.addObject(newsItem)
                     println( newsItem.newsTitle)
                 }
-                
+                if (self.dataSource.count != self.PAGESIZE) {
+                    self.loadingMoreShowing = false
+                } else {
+                    self.loadingMoreShowing = true
+                }
+                if (!self.loadingMoreShowing) {
+                    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+                }
                 dispatch_async(dispatch_get_main_queue(), {
-                    self.dataSource = currentNewsDataSource
+ //                   self.dataSource = currentNewsDataSource
                     self.tableView.reloadData()
-                    self.refreshControl.endRefreshing()
+                    
+                    if isRefresh {
+                        self.refreshControl.endRefreshing()
+                    } else {
+                        self.doneLoadingMoreTableViewData()
+                    }
                     })
             }
             })
+    }
+    
+    func doneLoadingMoreTableViewData() {
+        loadingMore = false
+        loadMoreFooterView!.loadMoreScrollViewDataSourceDidFinishedLoading(tableView)
     }
     
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         
-        var cell = tableView .dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        var cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+//        if !cell {
+//            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "cell")
+//        }
+        if(dataSource != nil && indexPath.row < dataSource.count) {
+            var newsItem = dataSource[indexPath.row] as XHNewsItem
+            cell.textLabel.text = newsItem.newsTitle
+            cell.imageView.image = UIImage(named :"iconpng")
+            cell.imageView.contentMode = UIViewContentMode.ScaleAspectFit
         
-        var newsItem = dataSource[indexPath.row] as XHNewsItem
-        cell.textLabel.text = newsItem.newsTitle
-        cell.imageView.image = UIImage(named :"iconpng")
-        cell.imageView.contentMode = UIViewContentMode.ScaleAspectFit
         
         
-        
-        let request = NSURLRequest(URL :NSURL.URLWithString(newsItem.newsThumb))
-        NSURLConnection.sendAsynchronousRequest(request, queue: thumbQueue, completionHandler: { response, data, error in
-            if error {
-                println(error)
+            let request = NSURLRequest(URL :NSURL.URLWithString(newsItem.newsThumb))
+            NSURLConnection.sendAsynchronousRequest(request, queue: thumbQueue, completionHandler: { response, data, error in
+                if error {
+                    println(error)
                 
-            } else {
-                let image = UIImage.init(data :data)
-                dispatch_async(dispatch_get_main_queue(), {
-                    cell.imageView.image = image
+                } else {
+                    let image = UIImage.init(data :data)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        cell.imageView.image = image
                     })
-            }
+                }
             })
         
-        
+        }
         return cell
     }
     
